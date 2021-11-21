d7a71300ecc5d66b8b21985ab07d5d1bc22212bc
Ianfranklyn <ian.franklyn@student.moringaschool.com>
2021-11-21 14:59:11 +0300

    Updated the css and Javascript

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,99 +1,61 @@
 $(document).ready(function(){
-    $("#development-image").click(function(){
-      $("#development-image").slideDown('1500').hide('1000');
-      $("#development").show('1500');
+
+
+  $("#text1").hide();
+    $("#icon1>img").click(function(){
+      $("#icon1>img").toggle().hide();
+      $("#text1").show();
     });
-    $("#development").click(function(){
-      $("#development").slideUp('1500');
-      $("#development-image").slideDown('1500');
+    $("#text1").click(function(){
+      $("#text1").hide();
+      $("#icon1>img").slideDown().show();
     });
+  
+  
+  $("#text2").hide();
+    $("#icon2>img").click(function(){
+      $("#icon2>img").toggle().hide();
+      $("#text2").show();
+    });
+    $("#text2").click(function(){
+      $("#text2").hide();
+      $("#icon2>img").slideDown().show();
+    });
+  
+  
+  $("#text3").hide();
+    $("#icon3>img").click(function(){
+      $("#icon3>img").toggle().hide();
+      $("#text3").show();
+    });
+    $("#text3").click(function(){
+      $("#text3").hide();
+      $("#icon3>img").slideDown().show();
+    });
+  
+  
+  $("#mouseclick").slideUp(800).slideDown(1200);
+  
+  $("#mouseclick").click(function(){
+    $("#about").show();
   });
   
-  $(document).ready(function(){
-    $("#design-image").click(function(){
-      $("#design-image").slideDown('1500').hide('1000');
-      $("#design").show('1500');
-    });
-    $("#design").click(function(){
-      $("#design").slideUp('1500');
-      $("#design-image").slideDown('1500');
-    });
-  });
   
-  $(document).ready(function(){
-    $("#product-image").click(function(){
-      $("#product-image").slideDown('1500').hide('1000');
-      $("#product").show('1500');
-    });
-    $("#product").click(function(){
-      $("#product").slideUp('1500');
-      $("#product-image").slideDown('1500');
-    });
-  });
-  $(document).ready(function(){
-    $("#work1").mouseover(function(){
-      $("#overlay").show();
-    }).mouseout(function(){
-      $("#overlay").hide();
-    });
-  });
-  $(document).ready(function(){
-    $("#work2").mouseover(function(){
-      $("#overlay2").show();
-    }).mouseout(function(){
-      $("#overlay2").hide();
-    });
-  });
-  $(document).ready(function(){
-    $("#work3").mouseover(function(){
-      $("#overlay3").show();
-    }).mouseout(function(){
-      $("#overlay3").hide();
-    });
-  });
-  $(document).ready(function(){
-    $("#work4").mouseover(function(){
-      $("#overlay4").show();
-    }).mouseout(function(){
-      $("#overlay4").hide();
-    });
-  });
   
-  $(document).ready(function(){
-    $("#work5").mouseover(function(){
-      $("#overlay5").show();
-    }).mouseout(function(){
-      $("#overlay5").hide();
-    });
-    $("#work6").mouseover(function(){
-      $("#overlay6").show();
-    }).mouseout(function(){
-      $("#overlay6").hide();
-    });
-    $("#work7").mouseover(function(){
-      $("#overlay7").show();
-    }).mouseout(function(){
-      $("#overlay7").hide();
-    });
-    $("#work8").mouseover(function(){
-      $("#overlay8").show();
-    }).mouseout(function(){
-      $("#overlay8").hide();
-    });
-  });
-  $(document).ready(function(){
-    $("form#form34A").submit(function(event){
-      event.preventDefault();
-      var name = $("input#MERGE1").val();
-      var email = $("input#MERGE0").val();
-      var message = $("textarea#comment").val();
-      if ($("input#MERGE1").val() && $("input#MERGE0").val()){
-        alert (name + ", we have received your message. Thank you for reaching out to us.");
-      }
-      else {
-        alert("Please enter your name and email!");
-      }
-      
-    });
+  
+  $('#form').submit(function(event){
+    event.preventDefault();
+  var user = $("user");
+    var email = $("#email");
+    var message = $("#message");
+    var submit = $("submit");
+  
+    if ($("#submit").val()){
+        alert ("Hello there!"+ " we have received your message. Thank you for reaching out to us.");                
+    }else{
+        alert ("Please submit your message appropriately.");
+    }
+  
+  })
   
   });
